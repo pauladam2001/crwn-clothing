901724cf911a5dae6988e7d5a1f64eb8d2474e23
911_Adam_Paul <72084877+pauladam2001@users.noreply.github.com>
2021-11-15 13:16:56 +0200

    Practiced container pattern in shop page

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,29 @@
 import ShopActionTypes from "./shop.types";
 
-export const updateCollections = (collectionsMap) => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
+import { firestore, convertCollectionSnapshotToMap } from "../../firebase/firebase.utils";
+
+export const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
     payload: collectionsMap
+});
+
+export const fetchCollectionsFailure = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage
 })
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections');
+        dispatch(fetchCollectionsStart());  // bcs of redux-thunk
+
+        collectionRef.get().then(snapshot => { 
+            const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+            dispatch(fetchCollectionsSuccess(collectionsMap));
+          }).catch(error => dispatch(fetchCollectionsFailure(error)));
+    }
+}
